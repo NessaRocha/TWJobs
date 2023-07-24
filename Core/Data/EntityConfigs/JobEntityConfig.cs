67baf3486f5fcc5d06afcfe4aaa117c24bc84728
Vanessa Rocha <87250768+NessaRocha@users.noreply.github.com>
2023-07-23 21:22:22 -0300

    .

---
+++ Core/Data/EntityConfigs/JobEntityConfig.cs
@@ -0,0 +1,26 @@
+using Microsoft.EntityFrameworkCore;
+using TWJobs.Core.Models;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace TWJobs.Core.Data.EntityConfigs;
+
+public class JobEntityConfig : IEntityTypeConfiguration<Job>
+{
+    public void Configure(EntityTypeBuilder<Job> builder)
+    {
+        builder.ToTable("Jobs");
+        builder.HasKey(j => j.Id);
+
+        builder.Property(j => j.Title)
+            .IsRequired()
+            .HasMaxLength(100);
+
+        builder.Property(j => j.Salary)
+            .IsRequired()
+            .HasPrecision(18, 2);
+
+        builder.Property(j => j.Requirements)
+            .IsRequired()
+            .HasMaxLength(500);
+    }
+}
